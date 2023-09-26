93b7e3136d32564b2aabe0cbf523cc1af9e83e95
zero400 <brooksroley@gmail.com>
2023-09-25 17:51:07 -0700

    Pente fixes.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,10 +22,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-2 text-2xl font-semibold`}>
+          <h2 className={`mb-2 text-2xl font-semibold`}>
             Github {' '} <span>-&gt;</span>
           </h2>
-          <p className={`${inter.className} text-sm mb-2`}>
+          <p className={`text-sm mb-2`}>
             Coding dreams into reality, one commit at a time! Explore my projects and contributions on GitHub. 💻✨
           </p>
           <Image
@@ -38,10 +38,10 @@ export default function Home() {
         </Link>
 
         <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/resume">
-          <h2 className={`${inter.className} mb-2 text-2xl font-semibold`}>
+          <h2 className={`mb-2 text-2xl font-semibold`}>
             Resume{' '}<span>-&gt;</span>
           </h2>
-          <p className={`${inter.className} text-sm mb-2`}>
+          <p className={`text-sm mb-2`}>
             Download link for interested parties, plus a little fun.
           </p>
           <Image src="/coverphoto.jpeg" alt="Brooks Roley" width={100} height={24} />
@@ -53,14 +53,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-2 text-2xl font-semibold`}>
+          <h2 className={`mb-2 text-2xl font-semibold`}>
             Play{' '}
             <span className="inline-block">
               -&gt;
             </span>
           </h2>
           <p
-            className={`${inter.className} text-sm mb-2`}
+            className={`text-sm mb-2`}
           >
             I am working on a game of Pente to play against an AI. I am still working on the AI so instead, here is a link my LolChess.
           </p>
@@ -83,14 +83,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-2 text-2xl font-semibold`}>
+          <h2 className={`mb-2 text-2xl font-semibold`}>
             Calendly{' '}
             <span className="inline-block transition-transform">
               -&gt;
             </span>
           </h2>
           <p
-            className={`${inter.className} text-sm mb-2`}
+            className={`text-sm mb-2`}
           >
             Schedule a time on my calendar
           </p>
@@ -113,11 +113,11 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-2 text-2xl font-semibold`}>
+          <h2 className={`mb-2 text-2xl font-semibold`}>
             Sensory Integration with CLASI{' '}<span>-&gt;</span>
           </h2>
           <p
-            className={`${inter.className} text-sm mb-2`}
+            className={`text-sm mb-2`}
           >
             Help childhood development and sensory integration by helping EASI. EASI is a non-profit organization that helps children with sensory integration issues.
           </p>
@@ -133,15 +133,11 @@ export default function Home() {
             />
           </div>
         </Link>
-
-        {/* <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/posts/first-post">First Post</Link> */}
-        {/* <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/posts/post-form">Post Form</Link> */}
-        <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/posts/Pente">Pente</Link>
         <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="https://www.linkedin.com/in/brooksroley/">
-          <h2 className={`${inter.className} mb-2 text-2xl font-semibold`}>
-            LinkedIn
+          <h2 className={`mb-2 text-2xl font-semibold`}>
+            LinkedIn{' '}<span>-&gt;</span>
           </h2>
-          <p className={`${inter.className} text-sm mb-2`}>
+          <p className={`text-sm mb-2`}>
             Unlocking professional potential and fostering meaningful connections. Let us connect on and make things happen! 🌟
           </p>
           <div className="relative flex place-items-center">
@@ -149,6 +145,26 @@ export default function Home() {
               className="relative"
               src="/water1.jpg"
               alt="Stairs to the Ocean in Japan"
+              width={180}
+              height={40}
+              priority
+            />
+          </div>
+        </Link>
+        {/* <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/posts/first-post">First Post</Link> */}
+        {/* <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/posts/post-form">Post Form</Link> */}
+        <Link className="bg-blue-900 text-white m-4 p-4 rounded" href="/posts/pente">
+          <h2 className='mb-2 text-2xl font-semibold'>
+            Pente{' '}<span>-&gt;</span>
+          </h2>
+          <p className={`text-sm mb-2`}>
+            A favorite strategy game I love to play. I am still working on the AI to play against so bear with me.
+          </p>
+          <div className="relative flex place-items-center">
+            <Image
+              className="relative"
+              src="/pente.jpg"
+              alt="Pente Board"
               width={180}
               height={40}
               priority
